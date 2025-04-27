ef7e60d86bb7d5c887661f360c1021d26a543c2f
inoIsNoob <inochoi@g.hongik.ac.kr>
2025-04-27 22:14:11 +0900

    #DFS #기초

--- DFS&BFS/음료수 얼려 먹기.py
+++ DFS&BFS/음료수 얼려 먹기.py
@@ -1,13 +1,13 @@
-def dfs(x, y):
+def checkHole(x, y):
   if x<0 or y<0 or x>=n or y>=m:
     return False
  
   if graph[x][y] == '0':
     graph[x][y] = '1'
-    dfs(x+1, y)
-    dfs(x-1, y)
-    dfs(x, y+1)
-    dfs(x, y-1)
+    checkHole(x+1, y)
+    checkHole(x-1, y)
+    checkHole(x, y+1)
+    checkHole(x, y-1)
     return True
   return False
 
@@ -20,7 +20,7 @@ for _ in range(n):
 
 for i in range(n):
   for j in range(m):
-    if dfs(i, j):
+    if checkHole(i, j):
       cnt += 1
 
 print(cnt)
