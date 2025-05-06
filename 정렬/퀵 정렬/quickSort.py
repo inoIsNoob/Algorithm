01853d8a9442ae9e86400259ff69fd798ba5f499
inoIsNoob <inochoi@g.hongik.ac.kr>
2025-05-06 22:18:09 +0900

    #퀵 정렬 #피벗 정렬 #어려워

---
+++ 정렬/퀵 정렬/quickSort.py
@@ -0,0 +1,25 @@
+def quickSort(arr, start, end):
+    if start >= end:
+        return
+    
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and arr[left] <= arr[pivot]:
+            left += 1
+        
+        while right > start and arr[right] >= arr[pivot]:
+            right -= 1
+        
+        if left > right:
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+        
+    quickSort(arr, start, right-1)
+    quickSort(arr, right+1, end)
+    
+arr = [5,7,9,0,3,1,6,2,4,8]
+quickSort(arr, 0, len(arr)-1)
+print(arr)
